4f0a2e71174d305330816e70659f826a20df4552
Lê Minh Bảo <besilent308@gmail.com>
2023-12-26 18:14:18 +0700

    Add new tab components

---
+++ src/main/java/me/leminhbao/view/components/tabs/TabPane.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package me.leminhbao.view.components.tabs;
+
+import javax.swing.JTabbedPane;
+
+/**
+ *
+ * @author Le Minh Bao
+ */
+public class TabPane extends JTabbedPane {
+
+    private TabTitle title;
+
+    public TabPane() {
+
+    }
+
+}
